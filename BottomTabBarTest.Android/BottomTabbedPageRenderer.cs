566152f1042b19e94e8937b622d27660be5e5a43
Depechie <glenn.versweyveld@gmail.com>
2018-12-18 13:39:43 +0100

    Move extension to class

--- BottomTabBarTest.Android/BottomTabbedPageRenderer.cs
+++ BottomTabBarTest.Android/BottomTabbedPageRenderer.cs
@@ -118,47 +118,4 @@ namespace BottomTabBarTest.Droid
             }
         }
     }
-
-    public static class AndroidViewUtils
-    {
-        public static void SetShiftMode(this BottomNavigationView bottomNavigationView, bool enableShiftMode, bool enableItemShiftMode)
-        {
-            try
-            {
-                using (var menuView = bottomNavigationView.GetChildAt(0) as BottomNavigationMenuView)
-                {
-                    if (menuView == null)
-                    {
-                        System.Diagnostics.Debug.WriteLine("Unable to find BottomNavigationMenuView");
-                        return;
-                    }
-
-                    var shiftMode = menuView.Class.GetDeclaredField("mShiftingMode");
-                    shiftMode.Accessible = true;
-                    shiftMode.SetBoolean(menuView, enableShiftMode);
-                    shiftMode.Accessible = false;
-                    shiftMode.Dispose();
-
-                    for (int i = 0; i < menuView.ChildCount; i++)
-                    {
-                        var child = menuView.GetChildAt(i);
-                        var item = child as BottomNavigationItemView;
-                        if (item != null)
-                        {
-                            item.SetShiftingMode(enableItemShiftMode);
-                            item.SetChecked(item.ItemData.IsChecked);
-                        }
-
-                        child.Dispose();
-                    }
-
-                    menuView.UpdateMenuView();
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine($"Unable to set shift mode: {ex}");
-            }
-        }
-    }
 }
--- BottomTabBarTest.Android/Extensions.cs
+++ BottomTabBarTest.Android/Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Android.Support.Design.Internal;
+using Android.Support.Design.Widget;
 using Android.Views;
 
 namespace BottomTabBarTest.Droid
@@ -30,6 +32,46 @@ namespace BottomTabBarTest.Droid
         {
             var childView = view.FindViewById(id);
             ((ViewGroup)childView.Parent).RemoveView(childView);
+        }
+
+        public static void SetShiftMode(this BottomNavigationView bottomNavigationView, bool enableShiftMode, bool enableItemShiftMode)
+        {
+            try
+            {
+                using (var menuView = bottomNavigationView.GetChildAt(0) as BottomNavigationMenuView)
+                {
+                    if (menuView == null)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Unable to find BottomNavigationMenuView");
+                        return;
+                    }
+
+                    var shiftMode = menuView.Class.GetDeclaredField("mShiftingMode");
+                    shiftMode.Accessible = true;
+                    shiftMode.SetBoolean(menuView, enableShiftMode);
+                    shiftMode.Accessible = false;
+                    shiftMode.Dispose();
+
+                    for (int i = 0; i < menuView.ChildCount; i++)
+                    {
+                        var child = menuView.GetChildAt(i);
+                        var item = child as BottomNavigationItemView;
+                        if (item != null)
+                        {
+                            item.SetShiftingMode(enableItemShiftMode);
+                            item.SetChecked(item.ItemData.IsChecked);
+                        }
+
+                        child.Dispose();
+                    }
+
+                    menuView.UpdateMenuView();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Unable to set shift mode: {ex}");
+            }
         }
     }
 }
